5e24562367b97a50e387b8b69461d8fe7201e106
Daniel Martí <mvdan@mvdan.cc>
2015-07-19 13:40:34 -0700

    Split up field types into fieldtypes.go

--- index.go
+++ index.go
@@ -4,13 +4,11 @@
 package fdroidcl
 
 import (
-	"encoding/hex"
 	"encoding/xml"
 	"fmt"
 	"io"
 	"sort"
 	"strings"
-	"time"
 )
 
 type Index struct {
@@ -26,57 +24,6 @@ type Repo struct {
 	Version     int    `xml:"version,attr"`
 	MaxAge      int    `xml:"maxage,attr"`
 	Description string `xml:"description"`
-}
-
-type CommaList []string
-
-func (cl *CommaList) FromString(s string) {
-	*cl = strings.Split(s, ",")
-}
-
-func (cl *CommaList) String() string {
-	return strings.Join(*cl, ",")
-}
-
-func (cl *CommaList) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var content string
-	if err := d.DecodeElement(&content, &start); err != nil {
-		return err
-	}
-	cl.FromString(content)
-	return nil
-}
-
-func (cl *CommaList) UnmarshalText(text []byte) (err error) {
-	cl.FromString(string(text))
-	return nil
-}
-
-type HexVal []byte
-
-func (hv *HexVal) FromString(s string) error {
-	b, err := hex.DecodeString(s)
-	if err != nil {
-		return err
-	}
-	*hv = b
-	return nil
-}
-
-func (hv *HexVal) String() string {
-	return hex.EncodeToString(*hv)
-}
-
-func (hv *HexVal) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var content string
-	if err := d.DecodeElement(&content, &start); err != nil {
-		return err
-	}
-	return hv.FromString(content)
-}
-
-func (hv *HexVal) UnmarshalText(text []byte) (err error) {
-	return hv.FromString(string(text))
 }
 
 // App is an Android application
@@ -105,35 +52,6 @@ type App struct {
 type HexHash struct {
 	Type string `xml:"type,attr"`
 	Data HexVal `xml:",chardata"`
-}
-
-type DateVal struct {
-	time.Time
-}
-
-func (dv *DateVal) FromString(s string) error {
-	t, err := time.Parse("2006-01-02", s)
-	if err != nil {
-		return err
-	}
-	*dv = DateVal{t}
-	return nil
-}
-
-func (dv *DateVal) String() string {
-	return dv.Format("2006-01-02")
-}
-
-func (dv *DateVal) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var content string
-	if err := d.DecodeElement(&content, &start); err != nil {
-		return err
-	}
-	return dv.FromString(content)
-}
-
-func (dv *DateVal) UnmarshalText(text []byte) (err error) {
-	return dv.FromString(string(text))
 }
 
 // Apk is an Android package
